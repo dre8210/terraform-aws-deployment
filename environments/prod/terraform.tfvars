99174ebb18ceb495120c46da4594aad8e05443d6
Carl Andrews Azafokpe <carlandrews8210@gmail.com>
2025-08-15 21:15:20 +0000

    Defined All Resources

--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,66 @@
 aws_region = "us-east-1"
+
+
+vpc_name_cidr = {
+  cidr_block = "10.0.0.0/16"
+  name = "dev_env_vpc"
+}
+
+private_subnet_1_config = {
+  cidr_block = "10.0.1.0/24"
+  az = "us-east-1a"
+}
+
+private_subnet_2_config = {
+  cidr_block = "10.0.3.0/24"
+  az = "us-east-1b"
+}
+
+public_subnet_1_config = {
+  cidr_block = "10.0.2.0/24"
+    az         = "us-east-1a"
+}
+
+public_subnet_2_config = {
+  cidr_block = "10.0.4.0/24"
+    az         = "us-east-1b"
+}
+
+sg_public_ssh = {
+  type = "ingress"
+  protocol = "tcp"
+  from_port = 22
+  to_port = 22
+  cidr_blocks = [ "0.0.0.0/0" ]
+  description = "Allow SSH access from anywhere"
+}
+
+sg_public_instance_egress  = {
+  type = "egress"
+  protocol = "-1"
+  from_port = 0
+  to_port = 0
+  cidr_blocks = [ "0.0.0.0/0" ]
+  description = "Allow Instance to access internet"
+}
+
+sg_public_alb_ingress = {
+  type = "ingress"
+  protocol = "tcp"
+  from_port = 80
+  to_port = 80
+  description = "Allow traffic from the ALB SG"
+}
+
+ec2_public = {
+  ami_id = "ami-020cba7c55df1f615"
+  instance_type = "t3.micro"
+  key_name = "terraform-keypair"
+}
+
+db_instance_config = {
+  db_name = "private_db"
+  instance_class = "db.t3.micro"
+  username = "foo"
+  password = "foobarbaz"
+}
--- environments/prod/variables.tf
+++ environments/prod/variables.tf
@@ -2,3 +2,116 @@ variable "aws_region" {
   type        = string
   description = "AWS Resource Region"
 }
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+}
+
+variable "vpc_name_cidr" {
+  description = "VPC name and CIDR Block"
+  type = object({
+    name       = string
+    cidr_block = string
+  })
+}
+
+variable "public_subnet_1_config" {
+  description = "Public subnet 1 config"
+
+  type = object({
+    cidr_block = string
+    az         = string
+  })
+
+}
+
+variable "public_subnet_2_config" {
+  description = "Public subnet 2 config"
+
+  type = object({
+    cidr_block = string
+    az         = string
+  })
+
+}
+
+variable "private_subnet_1_config" {
+  description = "Private subnet 1 config"
+
+  type = object({
+    cidr_block = string
+    az         = string
+  })
+}
+
+variable "private_subnet_2_config" {
+  description = "Private subnet 2 config"
+
+  type = object({
+    cidr_block = string
+    az         = string
+  })
+}
+
+variable "sg_public_ssh" {
+  description = "Security Group configuration for Public Subnet SSH"
+
+  type = object({
+    type        = string
+    protocol    = string
+    from_port   = number
+    to_port     = number
+    cidr_blocks = list(string)
+    description = string
+  })
+}
+
+variable "sg_public_instance_egress" {
+  description = "Security Group configuration for Public Subnet SSH"
+
+  type = object({
+    type        = string
+    protocol    = string
+    from_port   = number
+    to_port     = number
+    cidr_blocks = list(string)
+    description = string
+  })
+}
+
+variable "sg_public_alb_ingress" {
+  description = "Security Group configuration for Application Load Balancer Ingress"
+  type = object({
+    type                     = string
+    protocol                 = string
+    from_port                = number
+    to_port                  = number
+    source_security_group_id = optional(string)
+    description              = string
+  })
+}
+
+
+variable "ec2_public" {
+  description = "Configuration for a webserver in the public subnet"
+
+  type = object({
+    ami_id        = string
+    instance_type = string
+    key_name      = optional(string, null)
+
+  })
+}
+
+variable "db_instance_config" {
+  description = "Configuration fo RDS Instance"
+
+  type = object({
+    db_name        = string
+    instance_class = string
+    username       = string
+    password       = string
+  })
+}
+
+
